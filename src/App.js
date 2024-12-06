64d5365575f631d7eb575da2f5b066d04ca01312
ваше имя или ник латиницей <dmitry.bazyk@yandex.ru>
2024-12-06 15:41:27 +0700

    Первый коммит

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {/* Текущий год */}
+        <p>{new Date().getFullYear()}</p> {/* Добавлено отображение текущего года  это императивный подход */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
